77e9b47bdda5a5fe2c9affd8da1114e21be9efb9
Johnny Rangel <77410210+jrangel2295@users.noreply.github.com>
2022-07-19 16:11:40 -0500

    frontend

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import postRoutes from './routes/posts'
+
+// http://localhost:5000/posts
 
 const app = express()
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
---
+++ server/routes/posts.js
@@ -0,0 +1,9 @@
+import Express  from "express";
+
+const router = express.Router()
+
+router.get('/', (req,res)=>{
+    res.send('this works')
+})
+
+export default router;
