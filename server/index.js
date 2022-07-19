811b1621f676d2f8a45e574fde5c445a0160370a
Johnny Rangel <77410210+jrangel2295@users.noreply.github.com>
2022-07-19 15:43:59 -0500

    setting up backend

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const CONNECTION_URL =
+	'mongodb+srv://2224:Santna00@cluster0.izgkv.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
